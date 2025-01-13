fcf20d8eb0589730acd15c96ad7f558e785303df
Hilixx <jhonalizan@gmail.com>
2025-01-13 12:49:33 -0300

    peça service e cliente service

--- src/main/java/com/Attrezzo/services/ClienteService.java
+++ src/main/java/com/Attrezzo/services/ClienteService.java
@@ -1,5 +1,45 @@
 package com.Attrezzo.services;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Attrezzo.dtos.ClienteDto;
+import com.Attrezzo.models.Cliente;
+import com.Attrezzo.repositories.ClienteRepository;
+
+@Service
 public class ClienteService {
 
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	
+	public Cliente findById(UUID id) {
+		return clienteRepository.findById(id).orElseThrow(()-> new RuntimeException("cannot be found"));
+	}
+	public List<Cliente> findAll(){
+		return clienteRepository.findAll();
+	}
+	public Cliente createCliente(ClienteDto clienteDto) {
+		var cliente = new Cliente();
+		BeanUtils.copyProperties(clienteDto, cliente);
+		return clienteRepository.save(cliente);
+	}
+	public Cliente updateCliente(ClienteDto clienteDto, UUID id) {
+		var cliente = findById(id);
+		cliente.setCpf(clienteDto.cpf());
+		cliente.setEmail(clienteDto.email());
+		cliente.setNome(clienteDto.nome());
+		cliente.setTelefone(clienteDto.telefone());
+		return clienteRepository.save(cliente);
+	}
+	public void deleteCliente(UUID id) {
+		var cliente = findById(id);
+		clienteRepository.delete(cliente);
+	}
 }
--- src/main/java/com/Attrezzo/services/PecaService.java
+++ src/main/java/com/Attrezzo/services/PecaService.java
@@ -1,5 +1,42 @@
 package com.Attrezzo.services;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Attrezzo.dtos.PecaDto;
+import com.Attrezzo.models.Peca;
+import com.Attrezzo.repositories.PecaRepository;
+
+@Service
 public class PecaService {
 
+	@Autowired
+	PecaRepository pecaRepository;
+	
+	public Peca findById(UUID id) {
+		return pecaRepository.findById(id).orElseThrow(()-> new RuntimeException("cannot be found"));
+	}
+	public List<Peca> findAll(){
+		return pecaRepository.findAll();
+	}
+	public Peca createPeca(PecaDto pecaDto) {
+		var peca = new Peca();
+		BeanUtils.copyProperties(pecaDto, peca);
+		return pecaRepository.save(peca);
+	}
+	public Peca updatePeca(PecaDto pecaDto, UUID id) {
+		var peca = findById(id);
+		peca.setFuncao(pecaDto.funcao());
+		peca.setNome(pecaDto.nome());
+		peca.setQuantidade(pecaDto.quantidade());
+		return pecaRepository.save(peca);
+	}
+	public void deletePeca(UUID id) {
+		var peca = findById(id);
+		pecaRepository.delete(peca);
+	}
 }
