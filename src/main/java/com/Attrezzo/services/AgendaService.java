d2a5c0e4283445a6498b2647ef44abb9f56b7bdd
Hilixx <jhonalizan@gmail.com>
2025-01-14 08:48:54 -0300

    services finais

---
+++ src/main/java/com/Attrezzo/services/AgendaService.java
@@ -0,0 +1,45 @@
+package com.Attrezzo.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Attrezzo.dtos.AgendaDto;
+import com.Attrezzo.models.Agenda;
+import com.Attrezzo.repositories.AgendaRepository;
+
+@Service
+public class AgendaService {
+
+	
+	@Autowired
+	AgendaRepository agendaRepository;
+	
+	@Autowired
+	ClienteService clienteService;
+	
+	public Agenda findById(UUID id) {
+		return agendaRepository.findById(id).orElseThrow(()-> new RuntimeException("cannot be found"));
+	}
+	public List<Agenda> findAll(){
+		return agendaRepository.findAll();
+	}
+	public Agenda createAgenda(AgendaDto agendaDto) {
+		var agenda = new Agenda();
+		BeanUtils.copyProperties(agendaDto, agenda);
+		return agendaRepository.save(agenda);
+	}
+	public Agenda updateAgenda(AgendaDto agendaDto, UUID id) {
+		var agenda = findById(id);
+		agenda.setClienteId(clienteService.findById(agendaDto.clienteId()));
+		agenda.setData(agendaDto.data());
+		return agendaRepository.save(agenda);
+	}
+	public void deleteAgenda(UUID id) {
+		var agenda = findById(id);
+		agendaRepository.delete(agenda);
+	}
+}
---
+++ src/main/java/com/Attrezzo/services/PagamentoService.java
@@ -0,0 +1,45 @@
+package com.Attrezzo.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Attrezzo.dtos.PagamentoDto;
+import com.Attrezzo.models.Pagamento;
+import com.Attrezzo.repositories.PagamentoRepository;
+
+@Service
+public class PagamentoService {
+
+	
+	@Autowired
+	PagamentoRepository pagRepository;
+	
+	@Autowired
+	ClienteService clienteService;
+	
+	public Pagamento findById(UUID id) {
+		return pagRepository.findById(id).orElseThrow(()-> new RuntimeException("cannot be found"));
+	}
+	public List<Pagamento> findAll(){
+		return pagRepository.findAll();
+	}
+	public Pagamento createPagamento(PagamentoDto pagDto) {
+		var pagamento = new Pagamento();
+		BeanUtils.copyProperties(pagDto, pagamento);
+		return pagRepository.save(pagamento);
+	}
+	public Pagamento updatePagamento(PagamentoDto pagDto, UUID id) {
+		var pagamento = findById(id);
+		pagamento.setClienteId(clienteService.findById(pagDto.clienteId()));
+		pagamento.setValor(pagDto.valor());
+		return pagRepository.save(pagamento);
+	}
+	public void deletePagamento(UUID id) {
+		var pagamento = findById(id);
+		pagRepository.delete(pagamento);
+	}
+}
