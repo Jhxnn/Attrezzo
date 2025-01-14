d5f645512ecca8bc8f75c371254980ad60cd47b7
Hilixx <jhonalizan@gmail.com>
2025-01-14 12:54:42 -0300

    clienteController

--- src/main/java/com/Attrezzo/controllers/ClienteController.java
+++ src/main/java/com/Attrezzo/controllers/ClienteController.java
@@ -1,5 +1,52 @@
 package com.Attrezzo.controllers;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Attrezzo.dtos.ClienteDto;
+import com.Attrezzo.models.Cliente;
+import com.Attrezzo.services.ClienteService;
+
+@RestController
+@RequestMapping("/cliente")
 public class ClienteController {
 
+	
+	@Autowired
+	ClienteService clienteService;
+	
+	@GetMapping
+	public ResponseEntity<List<Cliente>> findAll(){
+		return ResponseEntity.status(HttpStatus.OK).body(clienteService.findAll());
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> findById(@PathVariable(name = "id")UUID id){
+		return ResponseEntity.status(HttpStatus.OK).body(clienteService.findById(id));
+	}
+	@PostMapping
+	public ResponseEntity<Cliente> createCliente(@RequestBody ClienteDto clienteDto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.createCliente(clienteDto));
+	}
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> updateCliente(@RequestBody ClienteDto clienteDto,
+			@PathVariable(name = "id")UUID id){
+		return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.updateCliente(clienteDto, id));
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Cliente> deleteCliente(@PathVariable(name = "id")UUID id){
+		clienteService.deleteCliente(id);
+		return ResponseEntity.noContent().build();
+	}
 }
